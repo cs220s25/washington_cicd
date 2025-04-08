8d2692d8caf31ee491057e8d62e90b3ff3a59f23
“Ronell <washingtonr@moravian.edu>
2025-04-08 08:15:40 -0400

    configure repo name

--- userdata.sh
+++ userdata.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 yum install git -y
-git clone https://github.com/cs220s25/<repo_name> /<repo_name>
-cd /<repo_name>
+git clone https://github.com/cs220s25/washington_cicd /washington_cicd
+cd /washington_cicd
 python3 -m venv .venv
 .venv/bin/pip install -r requirements.txt 
 cp web_counter.service /etc/systemd/system
